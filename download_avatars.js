ca906889a0809493ccd6260e41f6dbbf9a7db0e7
Jon Drew <jon.drew1492@gmail.com>
2017-11-06 19:55:24 +0000

    Print avatar URL.

--- download_avatars.js
+++ download_avatars.js
@@ -20,5 +20,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  JSON.parse(result).forEach(function(element) {
+    console.log(element["avatar_url"]);
+  })
 });
